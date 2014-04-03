d10d34321efe52328d4159c092416c77eef79548
crossjs <one@ffully.com>
2014-04-03 22:53:28 +0800

    init

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Aspect",
+        "Base"
+    ],
+    "modules": [
+        "Base"
+    ],
+    "allModules": [
+        {
+            "displayName": "Base",
+            "name": "Base",
+            "description": "基类"
+        }
+    ]
+} };
+});
